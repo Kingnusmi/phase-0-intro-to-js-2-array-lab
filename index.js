0cacddc47581fc7761d0552d09c37c21ddf65574
Kingnusmi <kingnusmi14@gmail.com>
2022-09-28 13:14:17 -0400

    commit tree a153a4cb44cd0831e0d533889a692aea1f8d9b65

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat() {
+    console.log (cats.push("Ralph"));
+}
+function destructivelyPrependCat() {
+    console.log (cats.unshift("Bob"));
+}
+function destructivelyRemoveLastCat() {
+    console.log (cats.pop());
+}
+function destructivelyRemoveFirstCat() {
+    console.log (cats.shift());
+}
+function appendCat() {
+    console.log (cats[0] = "Arnold");
+}
